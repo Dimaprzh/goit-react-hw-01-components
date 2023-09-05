e23124262bf37d2d5d614afe0dd92d899e51d0b8
Dimaprzh <dimaprzh@ukr.net>
2023-09-05 20:54:19 +0300

    1

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,30 @@
+
+
+
+
+export const Profile = ({ user }) => {
+    const { username, tag, location, avatar, stats: { followers, views, likes } } = user
+    return <div>
+        <div>
+            <img src={avatar} alt="User avatar" className="avatar" />
+            <h1>{username}</h1>
+            <p>@{tag}</p>
+            <p>{location}</p>
+        </div>
+
+        <ul>
+            <li>
+                <p>Followers</p>
+                <p>{followers}</p>
+            </li>
+            <li>
+                <p>Views</p>
+                <p>{views}</p>
+            </li>
+            <li>
+                <p>Likes</p>
+                <p>{likes}</p>
+            </li>
+        </ul>
+    </div>
+}
